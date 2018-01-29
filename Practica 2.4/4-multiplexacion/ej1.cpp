4428535821ecb401b35ba669ae71adf5b496a1a2
Sawyer13 <dgarci13@ucm.es>
2018-01-29 12:30:23 +0100

    Upload files

--- Practica 2.4/4-multiplexacion/ej1.cpp
+++ Practica 2.4/4-multiplexacion/ej1.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/select.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /*
 
@@ -24,28 +26,42 @@ using namespace std;
 int main(){
     fd_set rfds;
     struct timeval tv;
-    int retval;
-    char buffer[256];
+    int retval, fd;
+    char buff[256];
     /* Watch stdin (fd 0) to see when it has input. */
+    int tub1 = open("tuberia", O_CREAT|O_RDWR, 0666);
+    int tub2 = open("tuberia2", O_CREAT|O_RDWR, 0666);
 
+    
     FD_ZERO(&rfds);
-    FD_SET(0, &rfds);
-
+    FD_SET(tub1, &rfds);
+    FD_SET(tub2, &rfds);
+    
     /* Wait up to five seconds. */
 
     tv.tv_sec = 5;
     tv.tv_usec = 0;
 
-    retval = select(1, &rfds, NULL, NULL, &tv);
-    /* Don't rely on the value of tv now! */
+    if(tub1 > tub2)
+      fd = tub1;
+    else
+      fd = tub2;
+    
+    retval = select(fd+1, &rfds, NULL, NULL, &tv);
 
     if (retval == -1)
       perror("select()");
     else if (retval){
-      cout << "Data is available now." << "Datos retval: " << retval << endl;
+      cout << "Data is available now." << endl;
       /* FD_ISSET(0, &rfds) will be true. */
-      read(0, buffer, 80);
-      printf("Se ha recibido la cadena: %s\n", buffer);
+      if(FD_ISSET(tub1, &rfds)){
+        read(tub1, &buff, sizeof(buff));
+	cout << buff << endl;
+      }
+      else{
+	read(tub2, &buff, sizeof(buff));
+	cout << buff << endl;
+      }
     }
     else
       cout << "No data within five seconds." << endl;  
